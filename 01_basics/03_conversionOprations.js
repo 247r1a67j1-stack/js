0309b9958b79013a56490f8b7f685bdf96c6f4bc
247r1a67j1-stack <247r1a67j1@cmrtc.ac.in>
2026-06-06 12:54:53 +0000

    conversions

---
+++ 01_basics/03_conversionOprations.js
@@ -0,0 +1,29 @@
+let score=true
+
+
+
+//console.log(typeof score)
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+//"33"=>33
+//"33abc"=>NaN(Not an Number)
+// true=> 1,false=>0
+
+let isLoggedIn="hi"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true , 0=>false
+//""=>false
+//"sb"=>true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
