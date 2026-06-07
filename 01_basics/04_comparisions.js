a7795e6cbfd5d965ca17b91ac2acdf907764bded
247r1a67j1-stack <247r1a67j1@cmrtc.ac.in>
2026-06-07 10:21:24 +0000

    comparisons

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,29 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2<=1);
+//console.log(2!=1);
+
+
+//console.log("2">1);
+//console.log("02">1);
+
+//console.log(null>0);
+//console.log(null==0);
+//console.log(null>=0);
+
+//reason that an equality check == and comparisions ><>=<= work differentky:
+//comparisons convert null to anumber,treating it as 0
+
+
+//console.log(undefined==0);
+//console.log(undefined>=0);
+//console.log(undefined<=0);
+//console.log(undefined<0);
+
+
+// ===(strict check)
+
+console.log("2"===2);
+
+
