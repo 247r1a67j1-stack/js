406eb496acd01559bf233fbdc3f130b49ba27baf
247r1a67j1-stack <247r1a67j1@cmrtc.ac.in>
2026-06-08 14:33:55 +0000

    conversions

--- 01_basics/03_conversionOprations.js
+++ 01_basics/03_conversionOprations.js
@@ -16,7 +16,7 @@ let valueInNumber= Number(score)
 let isLoggedIn="hi"
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 //1=>true , 0=>false
 //""=>false
@@ -25,5 +25,60 @@ console.log(booleanIsLoggedIn)
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// OPERATIONS 
+
+let value=3
+let negval=-value
+//console.log(negval)
+
+let str1="hello"
+let str2=" srujana"
+
+let str3=str1+str2
+//console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log(1+2+"3");
+//console.log("1"+2+2);
+
+//console.log(true);
+
+let num1,num2,nu3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+//console.log(gameCounter);
+
+
+//      MEMORY
+
+// stack(premitive typed),heap(non oremitive)
+
+let myYoutubename="ncweifhwe.com"
+
+let anothername=myYoutubename
+anothername="chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@yabl"
+}
+
+let userTwo=userOne
+userTwo.email="sru@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
