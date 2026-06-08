6b3e3a97bdda1f2f058dd2baeb7981954004835d
247r1a67j1-stack <247r1a67j1@cmrtc.ac.in>
2026-06-08 14:33:31 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="shab@gmail.com"
+var accountPassword="12345"
+accountCity="Hyderabad"
+let accountState;
+
+// accountId=2
+
+accountEmail="hchc.com"
+accountPassword="334543"
+accountCity="bengaluru"
+
+/* prefer not to use var
+because of issue in block scope  and functional scope*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
