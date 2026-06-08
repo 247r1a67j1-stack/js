8e3ec7facd981489e53697841dfc8db972f1be5f
247r1a67j1-stack <247r1a67j1@cmrtc.ac.in>
2026-06-08 11:03:53 +0000

    dateandtime

---
+++ 01_basics/08_date_and_time.js
@@ -0,0 +1,51 @@
+//dates
+
+let myDate=new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toLocaleTimeString());
+//console.log(myDate.toTimeString());
+//console.log(myDate.toUTCString());
+//console.log(myDate.getTimezoneOffset());
+console.log(typeof myDate);
+
+//let myCreatedDate=new Date(2023,0,23);
+//console.log(myCreatedDate);
+
+//let myCreatedDate=new Date(2023,0,23,5,3);
+//console.log(myCreatedDate.toLocaleString());
+
+//let myCreatedDate=new Date("2023-01-14");
+//console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate=new Date("01-14-2023");
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+//console.log(myTimeStamp);
+
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+console.log(newDate.toLocaleString('default',{
+    weekday: "long",
+}));
+
+
+
+
+
+
+
+
+
+
+
