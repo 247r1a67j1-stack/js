20aab7c3958a7ea371a2060b3a87fd43d5f332e9
247r1a67j1-stack <247r1a67j1@cmrtc.ac.in>
2026-06-10 11:26:18 +0000

    object for each

---
+++ 05_iterations/four.js
@@ -0,0 +1,23 @@
+const myObject={
+    js:'javascript',
+    cpp:'C++',
+    rb:'ruby',
+    swift:'swift by app'
+}
+
+// for (const key in myObject) {
+// //    console.log(`${key} shortcut for${myObject[key]}`);
+// }
+
+// const program=["c++","python","java"]
+// for (const key in program) {
+//     console.log(key);
+    
+// }
+
+
+
+
+
+
+
