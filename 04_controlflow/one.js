8d7753ff1aec4826b96b858a2b8914e96dc32732
247r1a67j1-stack <247r1a67j1@cmrtc.ac.in>
2026-06-10 10:14:50 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,53 @@
+// if
+
+
+//const isUserLoggedIn=true;
+//const temparatue=41
+
+
+//if(temparatue<50){
+//console.log("less than 50");
+
+//}
+//else{
+  //  console.log("temparature greater than 50");
+    
+//}
+
+// <,>,<=,>=,==,!=,===(check datatype),
+
+
+//const score=200
+//if(score>200){
+  //  const power="fly"
+  //  console.log(`user power: ${power}`);
+    
+//}
+//console.log(`user power: ${power}`);
+
+
+//const balance=1000
+//if(balance>500) console.log("test");  //implicit scope
+
+//if (balance<500) {
+  //  console.log("less than");
+    
+//}else if(balance<750){
+ //   console.log("less than 750");
+    
+//}else{
+ //   console.log("less than 1200");
+    
+//}
+
+const userLoggedIn=true;
+const debitCard=true;
+if(userLoggedIn && debitCard){
+    console.log("allow to buy course");
+    
+}
+
+
+
+
+
